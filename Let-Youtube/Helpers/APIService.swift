1c8a95d718faa41180b1d9efeaa1763a429232bc
woonhyeong <tlsehddkql12@gmail.com>
2019-11-17 19:17:22 +0900

    Add APIService
    API에 요청하여 정보를 받아오는 함수를 모아놓은 class.
    
    fetchVideo(with:, completion:)은 url로 부터 video 정보를 받아온다.

---
+++ Let-Youtube/Helpers/APIService.swift
@@ -0,0 +1,46 @@
+//
+//  APIService.swift
+//  Let-Youtube
+//
+//  Created by 이운형 on 2019/11/17.
+//  Copyright © 2019 201302458. All rights reserved.
+//
+
+import UIKit
+
+class APIService: NSObject {
+    
+    static let shared = APIService()
+    
+    private override init() { super.init() }
+    
+    func fetchVideo(with URLString: String, completion: @escaping ([Video]) -> ()) {
+        guard let url = URL(string: URLString) else {
+            print("\(URLString) is invalid URL.")
+            return
+        }
+        
+        URLSession(configuration: .default).dataTask(with: url) {
+            data, response, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                var videos = [Video]()
+                let apiResponse = try JSONDecoder().decode([Video].self, from: data)
+                videos = apiResponse
+                
+                DispatchQueue.main.async {
+                    completion(videos)
+                }
+            } catch let jsonError {
+                print(jsonError.localizedDescription)
+            }
+        }.resume()
+    }
+}
---
+++ Let-Youtube/Helpers/URLInfo.swift
@@ -0,0 +1,13 @@
+//
+//  URLInfo.swift
+//  Let-Youtube
+//
+//  Created by 이운형 on 2019/11/17.
+//  Copyright © 2019 201302458. All rights reserved.
+//
+
+class URLInfo {
+    static let homeURL = "https://s3-us-west-2.amazonaws.com/youtubeassets/home.json"
+    static let trendingURL = "https://s3-us-west-2.amazonaws.com/youtubeassets/trending.json"
+    static let subscriptionURL = "https://s3-us-west-2.amazonaws.com/youtubeassets/subscriptions.json"
+}
